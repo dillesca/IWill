6d95d6155bf5a07b22a7a76764bc29f56433c96d
dillesca <illescas.dan@gmail.com>
2016-01-14 20:56:07 -0700

    Initial commit

---
+++ app/src/main/java/com/gxb/iwill/IWillFragment.java
@@ -0,0 +1,22 @@
+package com.gxb.iwill;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class IWillFragment extends Fragment {
+
+    public IWillFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_iwill, container, false);
+    }
+}
